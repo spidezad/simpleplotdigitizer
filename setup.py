6130d105c383f1043831f8f5fca7f42e14fd39f4
Kok Hua Tan <kokhuatan@Koks-MacBook-Air.local>
2019-07-08 22:51:21 +0800

    update 0.0.2 resolve error

--- setup.py
+++ setup.py
@@ -9,12 +9,9 @@ here = path.abspath(path.dirname(__file__))
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+    
 
-# get the dependencies and installs
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
-    all_reqs = f.read().split('\n')
-
-install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+install_requires = []
 dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
 
 setup(
